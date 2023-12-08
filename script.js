00c06df1b568c531ab3fb968653451a9d5db8e52
zraycd <vainiusvjg@gmail.com>
2023-12-08 14:10:53 +0200

    add timer for round

--- script.js
+++ script.js
@@ -1,74 +1,3 @@
-function getComputersChoice() {
-    let randomNumber = Math.random()
-    if (randomNumber > (2 / 3)) {
-        return 'rock'
-    } else if (randomNumber < (1 / 3)) {
-        return 'paper'
-    } else {
-        return 'scissors'
-    }
-}
-// let winCount = 0
-// let lossCount = 0
-// function playRound(playerSelection, computerSelection) {
-
-//     if (playerSelection != 'rock' && playerSelection != 'paper' && playerSelection != 'scissors') {
-//         return "Type in 'rock', 'paper', or 'scissors'!"
-//     }
-
-//     if (playerSelection === computerSelection) {
-//         return 'Its a draw! Both you and the computer selected ' + playerSelection
-//     }
-
-//     if (playerSelection === 'rock') {
-
-//         if (computerSelection === 'paper') {
-//             lossCount++
-//             return 'You lost! You selected rock and the computer selected paper'
-//         } else {
-//             winCount++
-//             return 'You win! You selected rock and the computer selected scissors'
-//         }
-
-//     } else if (playerSelection === 'paper') {
-
-//         if (computerSelection === 'scissors') {
-//             lossCount++
-//             return 'You lost! You selected paper and the computer selected scissors'
-//         } else {
-//             winCount++
-//             return 'You win! You selected paper and the computer selected rock'
-//         }
-
-//     } else if (playerSelection === 'scissors') {
-
-//         if (computerSelection === 'rock') {
-//             lossCount++
-//             return 'You lost! You selected scissors and the computer selected rock'
-//         } else {
-//             winCount++
-//             return 'You win! You selected scissors and the computer selected paper'
-//         }
-
-//     }
-// }
-// function game() {
-//     let i = 0
-//     while (i < 5) {
-//         let playerSelection = prompt('Round ' + (i + 1) + ': Rock, paper or scissors?').toLowerCase()
-//         let computerSelection = getComputersChoice()
-//         console.log(playRound(playerSelection, computerSelection))
-//         i++
-//     }
-//     if (winCount < lossCount) {
-//         console.log('You lost the game.')
-//     } else if (winCount > lossCount) {
-//         console.log('You won the game!')
-//     } else {
-//         console.log('Its a draw!')
-//     }
-// }
-// game()
 const rock = document.querySelector('#rock')
 const paper = document.querySelector('#paper')
 const scissors = document.querySelector('#scissors')
@@ -76,21 +5,46 @@ const container = document.querySelector('div')
 let computerSelection
 let playerSelection
 
+function getComputersChoice() {
+    let randomNumber = Math.random()
+    if (randomNumber > (2 / 3)) {
+        return '🪨'
+    } else if (randomNumber < (1 / 3)) {
+        return '📄'
+    } else {
+        return '✂️'
+    }
+}
 container.addEventListener('click', (event) => {
+    computerSelection = getComputersChoice()
+
     let target = event.target
+
+    console.log(target)
     switch(target.id) {
         case 'rock':
             playerSelection = 'rock'
+            document.querySelector('#computerSelection').innerText = computerSelection
+            paper.classList.add('hidden')
+            scissors.classList.add('hidden')
+            console.log('rock')
+            roundTimer()
             break;
         case 'paper':
             playerSelection = 'paper'
+            document.querySelector('#computerSelection').innerText = computerSelection
+            rock.classList.add('hidden')
+            scissors.classList.add('hidden')
+            roundTimer()
             break;
         case 'scissors':
             playerSelection = 'scissors'
+            document.querySelector('#computerSelection').innerText = computerSelection
+            paper.classList.add('hidden')
+            rock.classList.add('hidden')
+            roundTimer()
             break;
     }
-    computerSelection = getComputersChoice()
-    console.log(playRound(playerSelection, computerSelection))
 })
 
 function playRound(player, computer) {
@@ -106,4 +60,12 @@ function playRound(player, computer) {
             return computer === 'paper' ? 'You won!' : 'You lost.';
         
     }
+}
+function roundTimer() {
+    setTimeout(() => {
+        rock.classList.remove('hidden')
+        paper.classList.remove('hidden')
+        scissors.classList.remove('hidden')
+        document.querySelector('#computerSelection').innerText = '?'
+    }, 3000)
 }
